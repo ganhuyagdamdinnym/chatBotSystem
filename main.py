e7e8090f3d1175296393a7f76c285ea90cdbf44e
nymaa <damdinnymg@gmai.com>
2025-12-03 13:49:26 +0800

    refix

--- main.py
+++ main.py
@@ -10,10 +10,16 @@ Base_url = "https://api.openweathermap.org/data/2.5/weather"
 
 with open("./data.json", "r", encoding="utf-8") as f:
     INTENTS = json.load(f)
-def now_time():
-    now = datetime.datetime.now()
-    return f"{now.year} оны {now.month} сарын {now.day} өдөр {now.hour:02d}:{now.minute:02d}"
 
+def now_time():
+    months = [
+        "нэгдүгээр", "хоёрдугаар", "гуравдугаар", "дөрөвдүгээр",
+        "тавдугаар", "зургаадугаар", "долоодугаар", "наймдугаар",
+        "есдүгээр", "аравдугаар", "арван нэгдүгээр", "арван хоёрдугаар"
+    ]
+    now = datetime.datetime.now()
+    month_name = months[now.month - 1]
+    return f"{now.year} оны {month_name} сарын {now.day}-ны өдөр, {now.hour} цаг {now.minute} минут"
 
 app = FastAPI()
 
@@ -40,26 +46,18 @@ HELP_TEXT = (
     " - 'гомдол, санал'\n"
     " - 'цаг'\n"
     " - 'цаг агаар'\n"
-    "ℹ️ 'гарах' гэж бичвэл програм дуусна."
+   
 )
-
 
 class Message(BaseModel):
     text: str
 
-
 @app.post("/chat")
 def chat(message: Message):
     user_input = message.text
-   
     response = my_chatbot(user_input)
     return {"reply": response}
 
-
-# def my_chatbot(text: str):
-#     if "hello" in text.lower():
-#         return "Hi! How can I help you?"
-#     return "I didn’t understand that."
 def normalize(text: str) -> str:
     return " ".join(text.strip().lower().split())
 
@@ -71,32 +69,6 @@ def my_chatbot(user_input: str) -> str:
     if ui in ("цаг", "tsag", "time"):
         return f"Одоо цаг: {now_time()}"
 
-    # if ui in ("цаг агаар", "tsag agaar", "weather"):
-    #     lat = input("latitude: ")
-    #     lon = input("longitude: ")
-
-    #     url = f"{Base_url}?lat={lat}&lon={lon}&appid={Api_key}&units=metric"
-
-    #     response = requests.get(url)
-    #     data = response.json()
-
-    #     if response.status_code == 200:
-    #         city = data.get("name", "Unknown location")
-    #         temp = data["main"]["temp"]
-    #         weather = data["weather"][0]["description"].title()
-    #         humidity = data["main"]["humidity"]
-    #         wind = data["wind"]["speed"]
-
-    #         return (
-    #             f"\n Location: {city} ({lat}, {lon})"
-    #             f"\n Wind speed: {wind} m/s"
-    #             f"\n Temperature: {temp}°C"
-    #             f"\n Humidity: {humidity}%"
-    #             f"\n Weather: {weather}"
-    #         )
-    #     else:
-    #         return "Цаг агаарын мэдээллийг авахад алдаа гарлаа."
-
     for intent in INTENTS:
         for kw in intent["keywords"]:
             if normalize(kw) == ui:
@@ -104,11 +76,3 @@ def my_chatbot(user_input: str) -> str:
                 return ans(ui) if callable(ans) else ans
 
     return "Уучлаарай, ойлгосонгүй. 'тусламж' гэж бичээд боломжит асуултуудыг үзээрэй."
-
-
-# while True:
-#     user_input = input("Таны асуулт : ")
-#     if normalize(user_input) == "гарах":
-#         print("Баяртай")
-#         break
-#     print(chatbot_response(user_input))
